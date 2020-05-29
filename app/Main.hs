485f6f1df718e8d1dfef034a1f199f642e3e7bb0
Khan <khan.thompson@pointsbet.com>
2020-05-29 23:43:27 +1000

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Main where
+
+import           Control.Monad.Logger           ( runStdoutLoggingT
+                                                , logDebugSH
+                                                )
+import           Options.Applicative
+import           Lib                            ( startApp
+                                                , Arguments(..)
+                                                )
+
+main :: IO ()
+main = do
+  args <- execParser opts
+  runStdoutLoggingT $ $logDebugSH args
+  startApp args
+ where
+  opts = info
+    (argParser <**> helper)
+    (  fullDesc
+    <> progDesc "Provide either JSON or YAML for a YAML or JSON file"
+    <> header "roddles - I did it for you mate."
+    )
+
+argParser :: Parser Arguments
+argParser =
+  Arguments
+    <$> strOption
+          (short 'c' <> long "config-uri" <> metavar "config" <> help
+            "The blob storage uri to hit"
+          )
+    <*> option
+          auto
+          (  short 'p'
+          <> long "port"
+          <> metavar "port"
+          <> help "Port to run on"
+          <> value 8080
+          <> showDefault
+          )
